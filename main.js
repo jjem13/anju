f41ca1024e0b0b572dccac72dbc3c401bb0d2d4a
magni <133161506+jjem13@users.noreply.github.com>
2024-09-26 12:15:29 -0300

    d

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-let num =parseInt(prompt('numero'));
+let numero=parseInt(prompt('numero'));
 document.write(numero)
 function soma (num1,num2){
     return num1+num2;
 }
 console.log(soma(1,2));
+
 function areaQuadrado (lado){
     return lado**2;
 }
